041e98884e33330b64a2634787ddd96a8af42045
ConniConni <nmtgktmo@gmail.com>
2023-05-22 16:58:39 +0900

    Active Storageの導入

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,11 @@
 class Prototype < ApplicationRecord
   belongs_to :users
   has_many :comments
+  has_one_attached :image
+
+  #private
+
+  #def prototypes_params
+  #  params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
+  #end
 end
